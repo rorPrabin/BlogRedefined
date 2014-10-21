9eb8d440bb4a49d261d5517808d977f4677392da
rorPrabin <ror.prabin@gmail.com>
2014-10-21 17:56:01 +0545

    Added header, footer and css contents to the views.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,28 @@
 require 'test_helper'
-
 class StaticPagesControllerTest < ActionController::TestCase
+
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home Rails on Platform."
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help Rails on Platform."
   end
 
   test "should get about" do
-  	get :about
-  	assert_response :success
+    get :about
+    assert_response :success
+    assert_select "title", "About Rails on Platform."
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact Rails on Platform."
+  end
 end
