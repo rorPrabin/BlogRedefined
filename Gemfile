de451615447c4af983906cca37682091fb504895
rorPrabin <ror.prabin@gmail.com>
2014-10-22 15:38:44 +0545

    Added debugger interface to console and to the browser. By using bcrypt gem.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ ruby "2.0.0"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.6'
 # The bcrypt Ruby gem provides a simple wrapper for safely handling passwords.
-gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7', :require => 'bcrypt'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
 	has_many :microposts
 	before_save {self.email = email.downcase}
 	validates :name, length:{ maximum: 50}, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true. length:{maximum: 255},
-	format: { with: VALID_EMAIL_REGEX },
-	uniqueness: {case_sensitive: false}
-	has_secure_password
-	validates :password, length:{minimum:6}
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
