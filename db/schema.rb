e9aa0aaae1e84007decd8faf01d9e07ad37c01d5
rorPrabin <ror.prabin@gmail.com>
2014-10-22 14:39:20 +0545

    added users attributes and password digest in user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017093126) do
+ActiveRecord::Schema.define(version: 20141022083011) do
 
   create_table "microposts", force: true do |t|
     t.text     "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20141017093126) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
