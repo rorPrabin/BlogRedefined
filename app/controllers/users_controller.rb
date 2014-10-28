5658c104246989ca5656ed3c6e31ba44066e07fd
rorPrabin <ror.prabin@gmail.com>
2014-10-28 17:35:21 +0545

    Added admin migration And corrected edit with setting page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]#, :destroy,:show ]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]#,:show ]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   # GET /users 
   # GET /users.json
   def index
@@ -72,11 +73,14 @@ end
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url}#, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+    # @user.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to users_url}#, notice: 'User was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
@@ -84,6 +88,11 @@ end
     def set_user
       @user = User.find(params[:id])
     end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
     private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
