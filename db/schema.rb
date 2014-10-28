7913ba1b6a5debdc07902f6dbe7102832814e219
rorPrabin <ror.prabin@gmail.com>
2014-10-28 14:56:23 +0545

    Test on progress Till now

--- db/schema.rb
+++ db/schema.rb
@@ -23,10 +23,12 @@ ActiveRecord::Schema.define(version: 20141027074619) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
